85b213da1cc5b31d84b055ad0c21b06a60372574
David Amorim <david.amorim@phelma.grenoble-inp.fr>
2014-04-25 13:56:24 +0200

    Modification u4.h pour ajout des structures donnes

--- Quoridor/u4-fonctions.h
+++ Quoridor/u4-fonctions.h
@@ -7,22 +7,47 @@
 #define DUREE_CYCLE 0.015    // 0.500 secondes, depend du materiel utilise
 #define RAYON_BOULE  20
 
+//Declaration des enum
+
+enum TYPECASE{BARRIERE = 0, PION = 1};
+enum TYPEJOUEUR{HUMAIN = 0, ORDI = 1};
+enum COULEUR{BLANC = 0, NOIR = 1};
+
+
 // Declaration des donnees du projet
 
-// Structure de definition de la boule
-struct Boule
+// Structure de definition d'une case de damier
+struct Case
 {
-    int X;
-    int Y;
-    int VX;
-    int VY;
+    enum TYPECASE Type;
+    bool Occupe;
+    enum COULEUR Pion;
 } ;
+
+//Structure de définition d'un joueur
+struct Joueur
+{
+    int Ligne; //Ligne actuelle du pion jour
+    int Colonne; //Colonne actuelle du pion joueur
+    char image_pion[100]; //chargement fichier image pion
+    enum TYPEJOUEUR Type;
+    int NbBarrieres; //Nombre de barrières restant au joueur
+    int Chemin[TAILLEMAX][2] //Stockage du chemin optimal a parcourir
+    int NbCasesChemin; //Nb de cases du chemin le plus court
+    bool CheminTrouve; //Verification si chemin valide (n'enferme pas le pion)
+};
 
 // Structure globale pour les variables fonctionnelles
 struct Donnees
 {
-    struct Boule    Boule ;
+    struct Case Plateau[2N-1][2N-1];
+    struct Joueur JoueurBlanc;
+    struct Joueur JoueurNoir;
+
+    enum COULEUR JoueurEnCours;
 };
+
+
 
 extern struct Donnees gDonnees;
 
